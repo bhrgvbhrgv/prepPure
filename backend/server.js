28a4ee2e68d36b4e669a7d30a5bd6840ea1e376d
bhrgvbhrgv <bhargavdas21th@gmail.com>
2025-11-07 11:07:59 +0530

    added cors

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,31 @@ const connectDB = require('./src/db/db');
 
 connectDB();
 
+const allowedOrigins = ["https://prep-pure.vercel.app"];
+
+const corsOptionss = {
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  methods: "GET, POST, PUT, DELETE, HEAD",
+  credentials: true,
+  optionSuccessStatus: 200,
+};
+
+app.use((req, res, next) => {
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  }
+  next();
+});
+
+app.use(cors(corsOptionss));
+
 app.listen(3000,()=>{
     console.log("Server is running on port 3000");
 })
