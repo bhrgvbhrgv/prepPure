4bd3237b84539adebca7e24ded056ce6aee8a0af
bhrgvbhrgv <bhargavdas21th@gmail.com>
2025-11-07 12:16:38 +0530

    edited mongo db uri

--- backend/src/db/db.js
+++ backend/src/db/db.js
@@ -3,7 +3,7 @@
 const mongoose = require('mongoose');
 
 function connectDB(){
-    mongoose.connect("mongodb://localhost:27017/food-view").then(()=>{
+    mongoose.connect(process.env.MONGODB_URI).then(()=>{
         console.log("Connected to MongoDB successfully");
     }).catch((err)=>{
         console.log("Failed to connect to MongoDB", err);
